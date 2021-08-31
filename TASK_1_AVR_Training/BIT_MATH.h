2e84420b4a96a7082aeb0174c0f1abf6b2d9b43b
ayafarid <73356697+ayafarid@users.noreply.github.com>
2021-08-31 15:44:05 +0200

    Delete BIT_MATH.h

--- TASK_1_AVR_Training/BIT_MATH.h
+++
@@ -1,4 +0,0 @@
-#define SET_BIT(BYTE,BIT_NO) BYTE |= (0x01 << BIT_NO)
-#define CLEAR_BIT(BYTE,BIT_NO) BYTE &= ~(1 << BIT_NO)
-#define TOG_BIT(BYTE,BIT_NO) BYTE ^= (1 << BIT_NO)
-#define GET_BIT(BYTE,BIT_NO) (0x01 & (BYTE >> BIT_NO))
