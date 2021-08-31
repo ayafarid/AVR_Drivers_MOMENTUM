57376a6ab1dd16b204e61b5d374d06e964f2cba9
ayafarid <73356697+ayafarid@users.noreply.github.com>
2021-08-31 15:47:28 +0200

    Add files via upload

---
+++ TASK_1_AVR_Training/task1_2/BIT_MATH.h
@@ -0,0 +1,7 @@
+#ifndef BIT_MATHS_H
+#define BIT_MATHS_H
+#define SET_BIT(BYTE,BIT_NO) BYTE |= (0x01 << BIT_NO)
+#define CLEAR_BIT(BYTE,BIT_NO) BYTE &= ~(1 << BIT_NO)
+#define TOG_BIT(BYTE,BIT_NO) BYTE ^= (1 << BIT_NO)
+#define GET_BIT(BYTE,BIT_NO) (0x01 & (BYTE >> BIT_NO))
+#endif
