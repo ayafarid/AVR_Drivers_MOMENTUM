be3280807136fa31176985b5a8ce40d66fd6545c
ayafarid <73356697+ayafarid@users.noreply.github.com>
2021-09-12 16:45:33 +0200

    Add files via upload

---
+++ EXIN_TASK_SESSION7/2SWITCH_TASK1/MCAL/02-GIE/GIE_config.h
@@ -0,0 +1,7 @@
+
+#ifndef MCAL_02_GIE_GIE_CONFIG_H_
+#define MCAL_02_GIE_GIE_CONFIG_H_
+
+
+
+#endif
---
+++ EXIN_TASK_SESSION7/2SWITCH_TASK1/MCAL/02-GIE/GIE_interface.h
@@ -0,0 +1,10 @@
+#ifndef MCAL_02_GIE_GIE_INTERFACE_H_
+#define MCAL_02_GIE_GIE_INTERFACE_H_
+
+
+void GIE_voidEnable(void);
+void GIE_voidDisable(void);
+
+
+
+#endif /* MCAL_02_GIE_GIE_INTERFACE_H_ */
