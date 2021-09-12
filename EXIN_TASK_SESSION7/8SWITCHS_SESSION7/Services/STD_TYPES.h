c62bf6a6ec95359ccfbbb1ca900ef58187868865
ayafarid <73356697+ayafarid@users.noreply.github.com>
2021-09-12 18:54:33 +0200

    Add files via upload

---
+++ EXIN_TASK_SESSION7/8SWITCHS_SESSION7/Services/STD_TYPES.h
@@ -0,0 +1,29 @@
+#ifndef _STD_TYPES_H
+#define _STD_TYPES_H
+
+/*	Unsigned Type	*/
+typedef unsigned char 	       u8;
+typedef unsigned short int     u16;
+typedef unsigned long int 	   u32;
+typedef unsigned long long     u64;   
+
+/*	Signed Type		*/
+typedef signed char 	       s8;
+typedef signed short int       s16;
+typedef signed int             s32;
+typedef signed long long       s64;   
+
+/*	Float Type		*/
+typedef float                  f32;
+typedef double                 f64;
+typedef long double            f128;
+
+
+
+#define STD_OK					1
+#define STD_NOK					0
+
+
+#define NULL					0
+
+#endif
